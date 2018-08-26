b06b6cac5e966add9d369a522e5d58584ee16efd
kasumi64 <77638292@qq.com>
2018-08-26 08:28:05 +0800

    vue init
    新的vue库

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+	routes: [{
+			path: '/',
+			name: 'charts',
+			component: resolve => require(['../view/charts.vue'], resolve)
+		},
+		{
+			path: '/charts',
+			name: 'charts',
+			component: resolve => require(['../view/charts.vue'], resolve)
+		},
+		{
+			path: '/HelloWorld',
+			name: 'HelloWorld',
+			component: HelloWorld
+		}
+	]
+})
---
+++ src/view/charts.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<HChart :id="id" :option="option"></HChart>
+	</div>
+</template>
+
+<script>
+import HChart from '../components/high_bar.vue'; // 导入chart组件
+import options from '../view/charts.js'; // 导入chart组件模拟数据
+
+	export default {
+		name: 'charts',
+		data() {
+			let option = options.bar;
+			return {
+				id: 'test',
+				option: option
+			};
+		},
+		components: {
+			HChart
+		}
+	};
+</script>
+
+<style>
+	#test { width: 400px; height: 400px; margin: 40px auto; }
+</style>
